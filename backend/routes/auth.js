6c5d90a66d8bbef686625af0c536c8f3f8dd9719
maungmatubbar <maungmatubbar@gmail.com>
2023-03-13 15:49:45 +0600

    user login system and create jwt authtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -56,4 +56,35 @@ router.post(
     // );
   }
 );
+//User Login
+router.post(
+  "/login",
+  [body("email", "Enter a valid email").isEmail(), body("password", "Password is required").exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        res.status(400).json({ error: "User does not exists." });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        res.status(400).json({ error: "Password does not match." });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({
+        authToken: authToken,
+      });
+    } catch (error) {}
+  }
+);
 module.exports = router;
