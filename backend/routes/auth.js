30ddd282ba2f222edebed0f31b24361f94a1abeb
maungmatubbar <maungmatubbar@gmail.com>
2023-03-13 14:57:01 +0600

    Backend user password bcrypt and use jwt authtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const User = require("../Models/User");
 const { body, validationResult } = require("express-validator");
 const { json } = require("express");
-
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "Mongisagoodboy";
 router.post(
   "/create",
   [
@@ -18,20 +20,32 @@ router.post(
     }
 
     try {
-      const user = await User.findOne({ email: req.body.email });
+      let user = await User.findOne({ email: req.body.email });
+      console.log(user);
       if (user) {
         return res.status(400).json({
           error: "Sorry this user already exists.",
         });
       }
-      User.create({
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      // Create a new user
+      user = await User.create({
         name: req.body.name,
+        password: secPass,
         email: req.body.email,
-        password: req.body.password,
       });
-      return res.status(200).json({ message: "User created successfully" });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json(authToken);
+      // return res.status(200).json({ message: "User created successfully" });
     } catch (error) {
-      console.log(error.message);
+      console.log(error);
       res.status(400).send("Some error occured.");
     }
     // .then((user) => res.json(user))
